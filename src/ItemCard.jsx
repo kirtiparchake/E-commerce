e45b911871156e13b4caad19a2ab5a567f4cb63c
Kirti Parchake <kirtiparchake91@gmail.com>
2024-04-10 17:42:09 +0530

    placeorder function disable

--- src/ItemCard.jsx
+++ src/ItemCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ItemCard = ({product ,addToCart}) => {
+const ItemCard = ({product ,addToCart }) => {
   return (
     <div>
       <img src={product.image} alt="" />
       <div className="name">{product.name}</div>
       <div className="name">{product.price}</div>
       <button onClick={addToCart}>Add to Cart</button>
-      <button>order now</button>
+      <button >order now</button>
     </div>
   )
 }
